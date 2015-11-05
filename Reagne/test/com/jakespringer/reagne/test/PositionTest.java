08970cd42f4099925592791f28091704e2c2809f
Weirdbob95 <rory.soiffer@comcast.net>
2015-11-05 15:39:08 -0800

    no

--- Reagne/test/com/jakespringer/reagne/test/PositionTest.java
+++ Reagne/test/com/jakespringer/reagne/test/PositionTest.java
@@ -3,18 +3,21 @@ package com.jakespringer.reagne.test;
 import com.jakespringer.reagne.Reagne;
 import com.jakespringer.reagne.game.World;
 import com.jakespringer.reagne.gfx.LWJGLWindow;
+import java.io.File;
 
 public class PositionTest {
+
     public static void main(String[] args) throws InterruptedException {
+        System.setProperty("org.lwjgl.librarypath", new File("lib/lwjgl/native/windows").getAbsolutePath());
         final World world = new World();
-        
+
         final LWJGLWindow window = new LWJGLWindow();
         Reagne.initialize.forEach(x -> window.initialize(1200, 800, "Reagen Test"));
         Reagne.continuous.forEach(x -> window.update());
-        
+
 //        Input.whileKeyPressed(Keyboard.KEY_0).forEach(x -> System.out.println("Time elapsed: " + x));
         world.add(new Player());
-        
+
         Reagne.run();
     }
 }
